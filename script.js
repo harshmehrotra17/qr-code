e28fd3a324726203bab630b61c48dec864570ecf
harshmehrotra17 <125595739+harshmehrotra17@users.noreply.github.com>
2023-11-11 17:11:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function generateQRCode() {
+    var textInput = document.getElementById('textInput').value;
+    var qrcodeContainer = document.getElementById('qrcode');
+
+    if (textInput.trim() !== '') {
+        generateQRCodeFromText(textInput);
+    } else {
+        alert('Please enter text or URL.');
+    }
+}
+
+function generateQRCodeFromText(text) {
+    var qrcodeContainer = document.getElementById('qrcode');
+    
+    // Clear existing QR code
+    qrcodeContainer.innerHTML = '';
+
+    // Generate QR code
+    var qrcode = new QRCode(qrcodeContainer, {
+        text: text,
+        width: 128,
+        height: 128,
+    });
+}
+
+function downloadQRCode() {
+    var qrcodeContainer = document.getElementById('qrcode');
+    var img = qrcodeContainer.getElementsByTagName('img')[0];
+
+    if (img) {
+        var a = document.createElement('a');
+        a.href = img.src;
+        a.download = 'qrcode.png';
+
+        document.body.appendChild(a);
+        a.click();
+
+        document.body.removeChild(a);
+    } else {
+        alert('Generate a QR code first.');
+    }
+}
